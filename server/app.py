f3545d7c662e864796ff67f9fe51868e2f37b3c2
Frank Dillon <dillonf2@outlook.com>
2023-10-12 16:32:59 -0400

    flask chatterbox lab complete

--- server/app.py
+++ server/app.py
@@ -14,13 +14,38 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET','POST'])
 def messages():
-    return ''
+    if request.method =='GET':
+        msgs = Message.query.order_by(Message.created_at).all()
+        new_array=[msg.to_dict() for msg in msgs]
+        return jsonify(new_array), 200
 
-@app.route('/messages/<int:id>')
+    elif request.method == 'POST':
+        new_msg= Message(
+            id= request.json.get('id'),
+            body= request.json.get('body'),
+            username= request.json.get('username'),
+            created_at= request.json.get('created_at'),
+            updated_at= request.json.get('updated_at'),
+        )
+        db.session.add(new_msg)
+        db.session.commit()
+        response= make_response(new_msg.to_dict(), 200)
+        return response
+
+@app.route('/messages/<int:id>', methods=['PATCH','DELETE'])
 def messages_by_id(id):
-    return ''
+
+    if request.method== 'PATCH':
+        msg= Message.query.filter_by(id=id).first()
+        msg.body= request.json.get('body')
+        return make_response(msg.to_dict(), 200)
+
+    elif request.method=='DELETE':
+        msg= Message.query.filter_by(id=id).first()
+        db.session.delete(msg)
+        db.session.commit()
 
 if __name__ == '__main__':
     app.run(port=5555)
---
+++ server/migrations/versions/3ca483e2af1e_initial_migration_models_table_added.py
@@ -0,0 +1,24 @@
+"""initial migration models table added
+
+Revision ID: 3ca483e2af1e
+Revises: 0e12555d4c52
+Create Date: 2023-10-12 15:43:58.992419
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3ca483e2af1e'
+down_revision = '0e12555d4c52'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
